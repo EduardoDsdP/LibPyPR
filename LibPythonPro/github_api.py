e10e20acaae156cbd8435c3bdf91f53c227d9a19
Eduardodsdp <eduardodsdp@gmail.com>
2023-04-20 18:48:36 -0300

    Adicionada a informação em README. -Suportada para a versão 3 de python.

--- LibPythonPro/github_api.py
+++ LibPythonPro/github_api.py
@@ -1,10 +1,21 @@
-def buscar_avatar (Usuario):
+import requests
+
+def buscar_avatar (usuario):
     """
     Busca o avatar de um usuario no github
     :param Usuario: str com o nome de usuario no github
     :return: str com o link do avatar
     """
-    url= f'https://api.github.com/users/{Usuario}'
+    url= f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('EduardoDsdP'))
+
+
+
+
 
 
 
